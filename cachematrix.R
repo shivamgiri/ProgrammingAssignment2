5f643733775f2c95baa0988e89de053268b8fa55
shivamgiri <shivamgiri212@gmail.com>
2016-08-05 12:29:32 +0530

    Programming assignment 2 (New)
    Fixed the format of file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program makes use of two functions makeCacheMatrix and cacheSolve 
+## which is used to find inverse of an invertible matrix in an efficient manner
+## by making use of Lexical scoping in R.
 
-## Write a short comment describing this function
+## The function 'makeCacheMatrix' makes use of <<- operator and is used to create a list
+## A list containing functions to :
+##              1. set the matrix
+##              2. get the matrix
+##              3. set the inverse of matrix
+##              4. get the inverse of matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {  ## x: a square invertible matrix
+  
+  set<-function(y){
+    x<<- y                         ## using `<<-` to assign a value to an object in an environment 
+                                   ## different from the current environment. 
+    invrs<<-NULL
+  }
+  
+  get<-function()x
+  
+  setinverse<-function(inverse) invrs<<-inverse
+  
+  getinverse<-function() invrs
+  
+  list(set=set, get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
+  
 
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" function computes the inverse of the "matrix" returned by 
+## makeCacheMatrix(). If the inverse has already been calculated and the matrix
+## has not changed, it will return the inverse from the cache directly.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invrs<-x$getinverse()
+  
+  if(!is.null(invrs)){           ## Check if the inverse has already been calculated
+    print("Getting cached data") ## Provides satisfaction to the user
+    return(invrs)                ## Return inverse matrix
+  }
+  
+  mat<-x$get()                   ## In other case solving for inverse of matrix
+  
+  invrs<-solve(mat, ...)         ## solve function is used to calculate inverse of matrix
+  
+  x$setinverse(invrs)
+  
+  return(invrs)
+                                 ## Return a matrix that is the inverse of 'x'
 }
